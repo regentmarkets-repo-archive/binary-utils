999767f50b8eab3d69f109ff731ef8e6a0072c3c
qingwei <qingwei@binary.com>
2016-05-13 17:08:31 +0800

    Add test

---
+++ src/ticks/__tests__/historyToTicks-test.js
@@ -0,0 +1,37 @@
+import { expect } from 'chai';
+import historyToTicks from '../historyToTicks';
+
+describe('historyToTicks', () => {
+    const serverData = {
+        times: [
+            "1463130332",
+            "1463130334",
+            "1463130336",
+            "1463130338",
+            "1463130340",
+            "1463130342",
+            "1463130344",
+            "1463130346",
+            "1463130348",
+            "1463130350"
+        ],
+        prices: [
+            "675.1733",
+            "675.1620",
+            "675.2230",
+            "675.3117",
+            "675.3250",
+            "675.2880",
+            "675.1796",
+            "675.0785",
+            "675.0988",
+            "675.1587"
+        ],
+    };
+
+    it('should convert response from tick history call, to array of tick object', () => {
+        const actual = historyToTicks(serverData);
+        expect(actual).to.have.lengthOf(10);
+        expect(actual[0]).to.be.deep.equal({ epoch: 1463130332, quote: 675.1733 });
+    });
+});
--- src/ticks/historyToTicks.js
+++ src/ticks/historyToTicks.js
@@ -1,3 +1,5 @@
+
+// it's basically a zip + map, but I do not find we need it much, so no need to generalize
 export default history =>
     history.times.map((t, idx) => {
         const quote = history.prices[idx];
