fd863be65cf9fe86585c36fb2d674197c8f35c36
Boris Yankov <borisyankov@gmail.com>
2016-05-20 08:00:10 +0300

    Update several entries in contractCodeToText

--- src/trade/contractCodeToText.js
+++ src/trade/contractCodeToText.js
@@ -8,21 +8,22 @@ export default code => ({
     expiry_time: 'Expiry Time',
     exit_tick_time: 'Exit Time',
     date_settlement: 'Settlement Time',
-    sell_spot_time: 'Sell Time',
-    sell_time: 'Server Sell Time',
+    sell_time: 'Sell Time',
 
 	current_spot_time: 'Current Time',
 
 	ask_price: 'Ask Price',
 	bid_price: 'Bid Price',
 	current_spot: 'Current Spot',
-	entry_tick: 'Entry Tick',
 	entry_spot: 'Entry Spot',
 	exit_tick: 'Exit Spot',
 	sell_price: 'Exit Price',
 	buy_price: 'Entry Price',
-	sell_spot: 'Sell Spot',
 
 	barrier: 'Barrier',
 	barrier2: 'Low Barrier',
+
+	sell_spot_time: 'DO NOT USE',
+	entry_tick: 'DO NOT USE',
+	sell_spot: 'DO NOT USE',
 })[code];
