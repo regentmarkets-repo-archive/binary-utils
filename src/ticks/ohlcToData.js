84912f6d66aca2010a832b42f44e821c5d461411
qingwei <qingwei@binary.com>
2016-06-03 11:40:25 +0800

    Use open_time when available

--- src/ticks/ohlcToData.js
+++ src/ticks/ohlcToData.js
@@ -1,1 +1,2 @@
-export default d => [+d.epoch * 1000, +d.open, +d.high, +d.low, +d.close];
+// open_time is used when data is stream
+export default d => [+(d.open_time || d.epoch) * 1000, +d.open, +d.high, +d.low, +d.close];
