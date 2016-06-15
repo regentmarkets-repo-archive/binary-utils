4baebc7325eaf04f141f9c5d250de3917c23c214
qingwei <qingwei@binary.com>
2016-06-15 15:26:09 +0800

    Dumb down ticks array diff function

--- src/ticks/doTicksDifferJustOneEntry.js
+++ src/ticks/doTicksDifferJustOneEntry.js
@@ -1,12 +1,24 @@
 import doTicksEqual from './doTicksEqual';
 
 export default (ticks1, ticks2) => {
-    const lengthDiff = Math.abs(ticks1.length - ticks2.length);
+    const len1 = ticks1.length;
+    const len2 = ticks2.length;
+    const lengthDiff = Math.abs(len1 - len2);
 
-    if (lengthDiff !== 1) {
+    // case of 2 empty array
+    if (lengthDiff === 0 && len1 === 0) {
         return false;
     }
 
+    // case of [] vs [1]
+    if (lengthDiff === 1 && (len1 === 0 || len2 === 0)) {
+        return true;
+    }
+
+    if (lengthDiff > 1) {
+        return false;
+    }
+    
     return doTicksEqual(ticks1[ticks1.length - 1], ticks2[ticks2.length - 2])
         || doTicksEqual(ticks1[ticks1.length - 2], ticks2[ticks2.length - 1]);
 };
