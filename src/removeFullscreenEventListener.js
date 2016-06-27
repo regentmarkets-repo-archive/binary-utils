76d205981e714edfd1040a41536e9f1d199ee7d1
Qing Wei <qingwei@binary.com>
2016-06-27 18:28:48 +0800

    Qingwei/add full screen events (#18)
    * Abstract browser prefix for fullscreen event handler
    
    * Bump version & published

---
+++ src/removeFullscreenEventListener.js
@@ -0,0 +1,6 @@
+export default handler => {
+    document.removeEventListener('fullscreenchange', handler);
+    document.removeEventListener('webkitfullscreenchange', handler);
+    document.removeEventListener('mozfullscreenchange', handler);
+    document.removeEventListener('MSFullscreenChange', handler);
+}
