b90956edc44f3ac2dabb2ccfb2193c6e239df249
Boris Yankov <borisyankov@gmail.com>
2016-07-27 18:50:56 +0300

    Fix and add tests to numberToSignedString

--- src/number/__tests__/numberToSignedString-test.js
+++ src/number/__tests__/numberToSignedString-test.js
@@ -2,7 +2,15 @@ import { expect } from 'chai';
 import numberToSignedString from '../numberToSignedString';
 
 describe('numberToSignedString', () => {
-    it('Convert Number to a Signed string', () => {
-        expect(numberToSignedString(4567)).to.equal('+4567');
+    it('converts 0 directly to string', () => {
+        expect(numberToSignedString(0)).to.equal('0');
+    });
+
+    it('negative numbers have a minus sign', () => {
+        expect(numberToSignedString(-123)).to.equal('-123');
+    });
+
+    it('positive numbers have a plus sign', () => {
+        expect(numberToSignedString(123)).to.equal('+123');
     });
 });
