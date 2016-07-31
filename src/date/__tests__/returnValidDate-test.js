230d66bdbe3892bf8eefc5f3df4f57d00346674d
Nuru Salihu Abdullahi <nuruddeensalihu@yahoo.com>
2016-07-31 20:57:13 +0800

    Construct valid time and date form invalid time and dates (#29)

---
+++ src/date/__tests__/returnValidDate-test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import validDate from '../returnValidDate';
+
+describe('timeStringIsBetween', () => {
+	it('should construct a new valid date from the date with no days string', () =>{
+		const expectedDate = validDate('2016-07-', '-');
+		expect(expectedDate).to.equal('2016-07-01');
+	})
+
+	it('should construct a new valid date from the date with no month string', () =>{
+		const expectedDate = validDate('2016--24', '-');
+		expect(expectedDate).to.equal('2016-01-24');
+	})
+
+	it('should construct a new valid date from the date with no year string', () =>{
+		const expectedDate = validDate('-02-24', '-');
+		expect(expectedDate).to.equal('01-02-24');
+	})
+
+	it('should not temper with a valid date', ()=>{
+		const expectedDate = validDate('2016-07-28', '-');
+		expect(expectedDate).to.equal('2016-07-28');
+	})
+
+});
