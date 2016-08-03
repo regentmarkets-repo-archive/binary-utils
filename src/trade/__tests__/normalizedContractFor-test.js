16c5b2e493256ec3d529829021150ed9c6d18b0d
qingwei <qingwei@binary.com>
2016-08-03 15:40:00 +0800

    Add normalizedContractFor test

---
+++ src/trade/__tests__/normalizedContractFor-test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import normalizedContractFor from '../normalizedContractFor';
+import mockedContract from 'binary-test-data/contractsForGDAXI';
+
+describe('normalizedContractFor', () => {
+    it('accept a list ', () => {
+        expect(normalizedContractFor([])).to.not.throw;
+    });
+
+    it('when called with empty list as input, returns empty list', () => {
+        const normalized = normalizedContractFor([]);
+        expect(normalized).to.deep.equal({});
+    });
+
+    it(`
+        should return a tree like object with hierarchy as follow
+        -> category
+           -> type
+              -> [{...}]
+    `, () => {
+        const normalized = normalizedContractFor(mockedContract);
+        expect(normalized).to.contains.keys(['callput', 'touchnotouch']);
+        expect(normalized.callput).to.contains.keys(['CALL', 'PUT']);
+        expect(normalized.callput.CALL).to.have.lengthOf(4);
+    });
+});
+
