9eec3d4f4469bafdae3b6c90f1ecb2fa1468e8f1
Boris Yankov <borisyankov@gmail.com>
2016-08-10 11:40:56 +0300

    immutabeChildrenToJS can accept undefined as parameter

--- src/object/__tests__/immutableChildrenToJS-test.js
+++ src/object/__tests__/immutableChildrenToJS-test.js
@@ -3,23 +3,23 @@ import { fromJS } from 'immutable';
 import immutableChildrenToJS from '../immutableChildrenToJS';
 
 describe('immutableChildrenToJS', () => {
-    it('should throw when passed undefined', () => {
-        expect(() => immutableChildrenToJS()).to.throw();
+    it('returns undefined when no parameters', () => {
+        expect(immutableChildrenToJS()).to.equal(undefined);
     });
 
-    it('should return empty object with empty input', () => {
+    it('returns empty object with empty input', () => {
         const obj = {};
         const actual = immutableChildrenToJS(obj);
         expect(obj).to.deep.equal(actual);
     });
 
-    it('should return new object, and not transform the input parameter', () => {
+    it('returns new object, and does not transform the input parameter', () => {
         const obj = {};
         const actual = immutableChildrenToJS(obj);
         expect(obj).to.not.equal(actual);
     });
 
-    it('should return the same object if it has no immutable children', () => {
+    it('returns the same object if it has no immutable children', () => {
         const obj = {
             someProp: 'someVal',
             anotherProp: 123,
@@ -28,7 +28,7 @@ describe('immutableChildrenToJS', () => {
         expect(obj).to.deep.equal(actual);
     });
 
-    it('should transform immutable children of plain object', () => {
+    it('transforms immutable children of plain object', () => {
         const expected = {
             someObj: {
                 someProp: 'someVal',
--- src/object/immutableChildrenToJS.js
+++ src/object/immutableChildrenToJS.js
@@ -2,7 +2,7 @@ export const toPlainJS = obj =>
     obj && (typeof obj.toJS === 'undefined' ? obj : obj.toJS());
 
 export default obj =>
-    Object.keys(obj).reduce((acc, key) => {
+    obj && Object.keys(obj).reduce((acc, key) => {
         acc[key] = toPlainJS(obj[key]);
         return acc;
     }, {});
