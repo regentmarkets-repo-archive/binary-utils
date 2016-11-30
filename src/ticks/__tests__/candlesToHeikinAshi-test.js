2bc095a60bc2c5a613a088f2e43d6b6dc558c5a4
Boris Yankov <borisyankov@gmail.com>
2016-11-30 17:09:23 +0200

    Complete HeikinAshi and fix tests + publish

--- src/ticks/__tests__/candlesToHeikinAshi-test.js
+++ src/ticks/__tests__/candlesToHeikinAshi-test.js
@@ -23,7 +23,7 @@ describe('candlesToHeikinAshi', () => {
         { open: 1, high: 2, low: 3, close: 4 },
       ];
       const heikinAshi = [
-        { open: 0, high: 0, low: 0, close: 0 },
+        { open: 2.5, high: 2, low: 3, close: 2.5 },
       ];
       const result = candlesToHeikinAshi(candles).map(x => roundCandle(x, 2));
       expect(result).toEqual(heikinAshi);
@@ -34,7 +34,7 @@ describe('candlesToHeikinAshi', () => {
         { open: 56.67, high: 58.82, low: 57.03, close: 57.73 },
       ];
       const heikinAshi = [
-        { open: 56.67, high: 58.82, low: 57.03, close: 58.06 },
+        { open: 57.20, high: 58.82, low: 57.03, close: 57.56 },
       ];
       const result = candlesToHeikinAshi(candles).map(x => roundCandle(x, 2));
       expect(result).toEqual(heikinAshi);
@@ -46,8 +46,8 @@ describe('candlesToHeikinAshi', () => {
         { open: 57.46, high: 57.72, low: 56.21, close: 56.27 },
       ];
       const heikinAshi = [
-        { open: 56.67, high: 58.82, low: 57.03, close: 58.06 },
-        { open: 58.37, high: 58.37, low: 56.21, close: 56.92 },
+        { open: 57.20, high: 58.82, low: 57.03, close: 57.56 },
+        { open: 57.38, high: 57.72, low: 56.21, close: 56.92 },
       ];
       const result = candlesToHeikinAshi(candles).map(x => roundCandle(x, 2));
       expect(result).toEqual(heikinAshi);
--- src/ticks/candlesToHeikinAshi.js
+++ src/ticks/candlesToHeikinAshi.js
@@ -1,3 +1,10 @@
+const firstHeikinAshi = (candle: Candle) => ({
+  open: (candle.open + candle.close) / 2,
+  high: candle.high,
+  low: candle.low,
+  close: (candle.open + candle.high + candle.low + candle.close) / 4,
+});
+
 const singleHeikinAshi = (cur: Candle, prev: Candle) => ({
   open: (prev.open + prev.close) / 2,
   high: Math.max(cur.high, prev.open, prev.close),
@@ -5,13 +12,11 @@ const singleHeikinAshi = (cur: Candle, prev: Candle) => ({
   close: (cur.open + cur.high + cur.low + cur.close) / 4,
 });
 
-export default (candles: Candle[]): Candle[] => {
-  const ha = [];
-
-  candles.forEach((x, idx) => {
-    const prev = idx > 0 ? ha[idx - 1] : candles[0];
-    ha.push(singleHeikinAshi(x, prev));
-  });
-
-  return ha;
-};
+export default (candles: Candle[]): Candle[] =>
+  candles.reduce((list, x, idx) => {
+    const heikinAshi = idx === 0 ?
+      firstHeikinAshi(x) :
+      singleHeikinAshi(x, list[idx - 1]);
+    list.push(heikinAshi);
+    return list;
+  }, []);
